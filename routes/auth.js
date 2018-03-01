d48e97abb00855586a82d198cd7e3f407338fe95
Julien Noyer <dwsparis@MacBook-Pro-de-Julien.local>
2018-03-01 11:53:19 +0100

    Modification de la connexion FB

--- routes/auth.js
+++ routes/auth.js
@@ -10,36 +10,42 @@ Importer les composants de la route
 /*
 Configuration Passport
 */
-  // Variables d'environnement
-  require('dotenv').config();
+    // Variables d'environnement
+    require('dotenv').config();
 
-  // Passport : Facebook strategy
-  passport.use(new Strategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.CLIENT_CB_URL
+    // Passport : Facebook strategy
+    passport.use(new Strategy({
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: process.env.CLIENT_CB_URL,
+            // profileFields: ['id', 'displayName', 'photos', 'email']
+            profileFields: [
+                'email','gender','hometown','about','languages','location','work','website','religion','sports','inspirational_people','name','relationship_status','interested_in','public_key'
+            ]
 
-  }, (accessToken, refreshToken, profile, cb) => {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
+        }, (accessToken, refreshToken, profile, cb) => {
+            console.log(`User FB Token`, accessToken)
+            // In this example, the user's Facebook profile is supplied as the user
+            // record.  In a production-quality application, the Facebook profile should
+            // be associated with a user record in the application's database, which
+            // allows for account linking and authentication with other identity
+            // providers.
+            return cb(null, profile);
+        }
+    ));
 
-  // Passport : serializeUser
-  passport.serializeUser((user, cb) => { cb(null, user) });
-  
-  // Passport : deserializeUser
-  passport.deserializeUser((obj, cb) => { cb(null, obj) });
+    // Passport : serializeUser
+    passport.serializeUser((user, cb) => { cb(null, user) });
+
+    // Passport : deserializeUser
+    passport.deserializeUser((obj, cb) => { cb(null, obj) });
 //
 
 
 /*
 Définition des routes
 */
-    router.get('/facebook', passport.authenticate('facebook'));
+    router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
     router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/connexion' }), (req, res) =>{
         res.redirect('/');
